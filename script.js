95a3f5fb07fa0ea447a2dfdd2858f56458d79699
Liam <liammassey21@gmail.com>
2022-06-07 16:45:49 +1000

    Wiring JS to calculator interface

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const btn = document.querySelector('.calculator-buttons');
+const matches = document.querySelectorAll('div');
+const calcScreen = document.querySelector('.screen');
+console.log(matches);
+console.log(btn);
+
+
+
 function addition(a,b) {
     return a + b;
 }
@@ -27,3 +35,40 @@ function operate(operator, a, b) {
         return 'Incorrect input';
     }
 }
+
+/*function display() {
+    calcScreen.textContent = calculator;
+}*/
+
+btn.addEventListener('click', function(e) {
+    if (e.target.classList.contains('operator')) {
+        operator = e.target.value;
+        console.log(operator);
+    }
+    if (e.target.classList.contains('equals')) {
+        console.log('equals', e.target.value);
+    }
+    if (e.target.classList.contains('decimal')) {
+        console.log('decimal', e.target.value);
+    }
+    if (e.target.classList.contains('all-clear')) {
+        console.log('all-clear', e.target.value);
+    }
+    if (e.target.classList.contains('delete')) {
+        console.log('delete', e.target.value);
+    }
+    if (e.target.classList.contains('number')) {
+        inputNumber(e.target.value);
+        firstInt === '' ? firstInt = e.target.value : firstInt += e.target.value;
+    }
+});
+
+function inputNumber(num) {
+    return calcScreen.textContent === '0' ? calcScreen.textContent = num : calcScreen.textContent += num;
+}
+
+let operator = '';
+let firstInt = '';
+let secondInt = '';
+
+
